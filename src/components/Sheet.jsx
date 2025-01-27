928f7c10d35eb2caed89ec6664a20e070e6cef18
leoshliak <139138564+leoshliak@users.noreply.github.com>
2025-01-27 12:50:45 +0200

    Full rework of Sheet.jsx

--- src/components/Sheet.jsx
+++ src/components/Sheet.jsx
@@ -1,17 +1,56 @@
-export default function Sheet() {
-   return (
-    <>
-     <div className="paper-sheet">
+import React from "react";
+
+const Sheet = ({name ,email, phone, adress, visibleDivs, edarr, exparr}) => {
+    return (
+        <div className="paper-sheet">
       <div className="personal-info">
-         <h3>James Milner</h3>
+         <h3>{name}</h3>
          <div className="personal-data">
-            <p>example@gmail.com</p>
-            <p>+38 094 343 1122</p>
-            <p>UK, Liverpool</p>
+            <p>{email}</p>
+            <p>{phone}</p>
+            <p>{adress}</p>
          </div>
       </div>
-      <div className="rest"></div>
+      <div className="rest">
+        <div className="education-data">
+            <h4 style={{ display: edarr.length > 0 || visibleDivs.edForm  ? 'block' : 'none' }}>Education</h4>
+            <div className="educations-container">
+            {edarr.map((educ, index) => (
+            <div className="ed-piece" key={index}>
+              <div>
+              <p className="ed-title">{educ.EI}</p>
+              <p className='degree'>{educ.degree}</p>
+              </div>
+              <div>
+              <p>{educ.startDate} – {educ.endDate}</p>
+              <p>{educ.location}</p>
+              </div>
+            </div>
+))}</div>
+        </div>
+          <div className="experience-data">
+          <h4 style={{ display: exparr.length > 0 || visibleDivs.expForm  ? 'block' : 'none' }}>Professional Experience</h4>
+          <div className="experience-container">
+            {exparr.map((exp, index) => (
+              <div className="exp-piece" key={index}>
+                <div className='no-des-data'>
+                   <div>
+                <p className='exp-title'>{exp.compName}</p>
+                <p>{exp.position}</p>
+                  </div>
+                  <div>
+                <p>{exp.startDate} – {exp.endDate}</p>
+                <p>{exp.location}</p>
+                </div>
+                </div>
+                <p className='des'>{exp.description}</p>
+              </div>
+            ))}
+          </div>
+          </div>
+      </div>
      </div>
-    </>
-   )
+    )
 }
+
+export default Sheet
